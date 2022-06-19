cf37bbcbdec0f344d3ae4d5b1feac777c5b16f4a
DanTaylor28 <dan.taylor1493@gmail.com>
2022-06-19 09:04:12 +0000

    Declare constants for dom elements and list out required functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,18 @@
 
-    let buttons = document.getElementsByTagName('button');
-    let userScore = document.getElementById('your-tally');
-    let compScore = document.getElementById('comp-tally');
+    let userScoreEl = document.getElementById('your-tally');
+    let compScoreEl = document.getElementById('comp-tally');
+    const userScore = 0;
+    const compScore = 0;
+    const rockButton = document.getElementById('rock-button');
+    const paperButton = document.getElementById('paper-button');
+    const scissorsButton = document.getElementById('scissors-button');
+    const resetButton = document.getElementById('reset-button');
 
-    for (let button of buttons) {
-        button.addEventListener('click', function() {
-            let playerChoice = this.getAttribute('data-type');
-            alert ('buttons are connected')
-        })
-    }
+    rockButton.addEventListener('click', function() {
+        console.log('you clicked on rock');
+    })
+
+
 
    
 
